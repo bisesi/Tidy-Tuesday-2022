8541999add6d4daeda04501a92dbb8803f9e8e60
Ave Bisesi <bises004@umn.edu>
2022-02-22 19:14:14 -0600

    updates

--- Week 5/Week5-dog-breeds.R
+++ Week 5/Week5-dog-breeds.R
@@ -2,13 +2,14 @@
 #TidyTuesday W5 2022
 #Dog Breeds
 
-library(tidytuesdayR)
-library(tidyverse)
-library(ggradar)
-library(scales)
-library(showtext)
-library(stringr)
-library(cowplot)
+#load packages
+library("tidytuesdayR")
+library("tidyverse")
+library("ggradar")
+library("scales")
+library("showtext")
+library("stringr")
+library("cowplot")
 
 #import dataset
 breed_traits <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-02-01/breed_traits.csv') %>%
@@ -17,8 +18,8 @@ trait_description <- readr::read_csv('https://raw.githubusercontent.com/rfordata
 breed_rank_all <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-02-01/breed_rank.csv') %>%
   mutate(Breed = str_squish(Breed)) %>% select(Breed, "2020 Rank")
 
-#create data for plot
-data<-breed_traits%>%
+#Clean data for plot
+data <- breed_traits%>%
   left_join(breed_rank_all, by = "Breed") %>%
   head(10) %>%
   select("Breed","Affectionate With Family", "Good With Young Children", 
@@ -42,14 +43,18 @@ data<-breed_traits%>%
                            Breed == "Pointers (German Shorthaired)" ~ "Short\nHaired\nPointer",
                            Breed == "Dachshunds" ~ "Dachshund"))
 
-#create factor to help order ggplot facet (will default to alphabetical)
-data$Breed<-factor(data$Breed, levels=data$Breed)
+#Get factors in descending order (avoid ggplot default to alphabetical)
+data$Breed <- factor(data$Breed, levels=data$Breed)
 
-#get names of columns
+#Get names of columns, replace for radar plot use
 names(data)[1]<-"group"
 
+#download fonts
+font_add_google("Cabin")
+showtext_auto()
+
 #radar plot
-Mainplot <- data%>%
+Mainplot <- data %>%
   ggradar(
     grid.min = 1, grid.mid = 3, grid.max = 5,
     background.circle.colour = "white",
@@ -67,8 +72,8 @@ Mainplot <- data%>%
     axis.label.size=3,
     plot.extent.x.sf = 1.5,
     plot.extent.y.sf = 1.5,
-    group.colours = "#117733",
-    font.radar = "Montserrat"
+    group.colours = "darkred",
+    font.radar = "Cabin"
   )+
   facet_wrap(vars(group), ncol = 5)+
   labs(title="Personalities of The Most Popular Dog Breeds",
@@ -77,14 +82,14 @@ Mainplot <- data%>%
   theme_void()+
   theme(legend.position="none",
         plot.background = element_rect(fill = "white"),
-        plot.subtitle= element_text(family = "Montserrat", hjust=0.5, margin=margin(0,0,30,0)),
-        plot.caption=element_text(family = "Montserrat", size=10,  face = "italic"),
-        strip.text = element_text(size = 10,  family = "Montserrat", face = "bold"),
+        plot.subtitle= element_text(family = "Cabin", hjust=0.5, margin=margin(0,0,30,0)),
+        plot.caption=element_text(family = "Cabin", size=10),
+        strip.text = element_text(size = 10,  family = "Cabin", face = "bold"),
         plot.margin=margin(20,0,10,0),
-        plot.title=element_text(family = "Montserrat", hjust=0.5, face="bold", size=18, margin=margin(0,0,10,0)))
+        plot.title=element_text(family = "Cabin", hjust=0.5, face="bold", size=18, margin=margin(0,0,10,0)))
 
 #Annotated plot
 ggdraw(Mainplot) +
-  draw_image("/Users/abisesi/Desktop/PhD/Non-Dissertation Work/Programming Repos/R/Visualizations/Tidy Tuesday/AKC.jpeg",
+  draw_image("/Users/abisesi/Desktop/PhD/Non-Dissertation Work/Programming Repos/R/Tidy-Tuesday-2022/Week 5/AKC.png",
              x = 0.9, y = 0.1, hjust = 1, vjust = 1, 
              halign = 1, valign = 1, width = 0.1)
